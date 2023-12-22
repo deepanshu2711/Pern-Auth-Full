3866974f798b0167d0785657161102b49d579c00
Deepanshu <deepanshusaini2711@gmail.com>
2023-12-22 22:04:45 +0530

    install react js and tailwindcss and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className='bg-blue-950'>Deepanshu</h1>
+    </div>
+  )
+}
+
+export default App
