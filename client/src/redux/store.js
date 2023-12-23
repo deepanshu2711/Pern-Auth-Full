10e8292130cac2832040df6ede29045e951ee1e3
Deepanshu <deepanshusaini2711@gmail.com>
2023-12-23 19:11:50 +0530

    add redus persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import  useReducer  from "./user/slice.js";
+import {persistReducer ,persistStore} from "redux-persist";
+import { config } from "dotenv";
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({user:useReducer});
+const persistConfig = {
+    key :'root',
+    version :1,
+    storage
+}
+const persistedReducer = persistReducer(persistConfig , rootReducer)
 
 export const store = configureStore({
-    reducer :{user:useReducer},
+    reducer :persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck:false
     }),
     
 });
+
+export const persistor =persistStore(store)
