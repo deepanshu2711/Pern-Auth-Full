a23d41f7e8260c22e2deb0159b9376a5e9ab975e
Deepanshu <deepanshusaini2711@gmail.com>
2023-12-23 14:50:27 +0530

    sreate signin api route

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -1,5 +1,9 @@
 import db from "../db.Config.js"
 import bcryptjs from "bcryptjs";
+import {errorHandler} from "../utils/error.js"
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const signup = async(req,res ,next) =>{  // req is the data we get from the client side 
     const{username ,email,password} = req.body;
@@ -10,6 +14,24 @@ export const signup = async(req,res ,next) =>{  // req is the data we get from t
 
     } catch (error) {                                           
         next(error);  //pass the error to the next middleware
+    }
+}
+
+export const signin = async(req,res,next)=>{
+    const {email , password} = req.body;
+    try {
+        const validuser = await db.query("select * from users where email =$1",[email]);
+        if(validuser.rows.length ==0){
+           return  next(errorHandler(401,'User not found'));
+        }
+        const validPassword = bcryptjs.compareSync(password,validuser.rows[0].password);
+        if(!validPassword) return next(errorHandler(401 ,"Wrong credentials"));
+        const token = jwt.sign({id :validuser.rows[0].id} ,process.env.JWT_Secret);
+        // const{password :hashedPassword ,...rest} = validuser.rows[0]; // if you dont to send password
+        res.cookie('access_token', token ,{httpOnly :true}).status(200).json(validuser.rows[0]);
+
+    } catch (error) {
+        next(error)
     }
 }
 
--- api/routes/auth_route.js
+++ api/routes/auth_route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { signup } from "../controllers/auth_controller.js";
+import { signin, signup } from "../controllers/auth_controller.js";
 
 const router = express.Router();
 
 
-router.post("/signup" , signup)
+router.post("/signup" , signup);
+router.post("/signin" ,signin)
 
 
 
